cdecbda95e5b603b6edd512a9fe66952b0a0bec6
riyo555 <riyoekakarunia@gmail.com>
2025-06-28 22:44:28 +0700

    Update api.php
    d

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/mahasiswa', MahasiswaController::class);
+Route::get('/test-api', function () {
+    return response()->json(['status' => 'API OK']);
+});
